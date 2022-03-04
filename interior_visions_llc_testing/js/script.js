a38cdf284fd4a7293c791d030f97c8f6329ee702
lmckinley28 <lmckinley@eastsideprep.org>
2022-03-04 08:51:04 -0800

    work

--- interior_visions_llc_testing/js/script.js
+++ interior_visions_llc_testing/js/script.js
@@ -1,130 +1,132 @@
-! function(n) {
-    ! function() {
-        for (var n = 0, i = [
-                "ms", "moz", "webkit", "o"
-            ], a = 0; a < i.length && !window.requestAnimationFrame; ++a)
-            window.requestAnimationFrame = window[i[a] + "RequestAnimationFrame"],
-            window.cancelAnimationFrame = window[i[a] + "CancelAnimationFrame"] || window[i[a] + "CancelRequestAnimationFrame"];
-
-        window.requestAnimationFrame || (window.requestAnimationFrame = function(i) {
-                var a = (new Date).getTime(),
-                    t = Math.max(0, 16 - (a - n)),
-                    o = window.setTimeout(function() {
-                        i(a + t)
-                    }, t);
-                return n = a + t,
-                    o
-            }),
-            window.cancelAnimationFrame || (window.cancelAnimationFrame = function(n) {
-                clearTimeout(n)
-            })
-    }(),
-    n.fn.sakura = function(i) {
-        function a(n, i) {
-            return Math.floor(Math.random() * (i - n + 1)) + n
-        }
-
-        function t(n, i, a) {
-            for (var t = 0; e > t; t++)
-                o[t] || (i = i.toLowerCase()),
-                n.get(0).addEventListener(o[t] + i, a, !1)
-
-        }
-        var o = [
-                "moz",
-                "ms",
-                "o",
-                "webkit",
-                ""
-            ],
-            e = o.length,
-            m = {
-                blowAnimations: [
-                    "blow-soft-left",
-                    "blow-medium-left",
-                    "blow-hard-left",
-                    "blow-soft-right",
-                    "blow-medium-right",
-                    "blow-hard-right"
-                ],
-                className: "sakura",
-                fallSpeed: 0.3,
-                maxSize: 17,
-                minSize: 10,
-                newOn: 300,
-                swayAnimations: [
-                    "sway-0",
-                    "sway-1",
-                    "sway-2",
-                    "sway-3",
-                    "sway-4",
-                    "sway-5",
-                    "sway-6",
-                    "sway-7",
-                    "sway-8"
-                ]
+tsParticles.load("tsparticles", {
+    fpsLimit: 60,
+    particles: {
+        number: {
+            value: 0,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ff0000",
+            animation: {
+                enable: true,
+                speed: 180,
+                sync: true
+            }
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
             },
-            i = n.extend({}, m, i),
-            r = n(document).height(),
-            s = n(document).width(),
-            w = n('<div class="' + i.className + '" />');
-        var d = function() {
-            setTimeout(function() {
-                requestAnimationFrame(d)
-            }, i.newOn);
-            var o = i.blowAnimations[Math.floor(Math.random() * i.blowAnimations.length)],
-                e = i.swayAnimations[Math.floor(Math.random() * i.swayAnimations.length)],
-                m = (Math.round(.007 * r) + 5 * Math.random()) * i.fallSpeed,
-                l = "fall " + m + "s linear 0s 1, " + o + " " + (
-                    (
-                        m > 30 ?
-                        m :
-                        30
-                    ) - 20 + a(0, 20)
-                ) + "s linear 0s infinite, " + e + " " + a(2, 4) + "s linear 0s infinite",
-                u = w.clone(),
-                c = a(i.minSize, i.maxSize),
-                h = Math.random() * s - 100,
-                f = -(20 * Math.random() + 15);
-            t(u, "AnimationEnd", function() {
-                    n(this).remove()
-                }),
-                t(u, "AnimationIteration", function(a) {
-                    -1 != n.inArray(a.animationName, i.blowAnimations) && n(this).remove()
-                }),
-                u.css({
-                    "-webkit-animation": l,
-                    "-o-animation": l,
-                    "-ms-animation": l,
-                    "-moz-animation": l,
-                    animation: l,
-                    height: c,
-                    left: h,
-                    "margin-top": f,
-                    width: c
-                }).appendTo(".sakuraT")
-        };
-        n(window).resize(function() {
-                r = n(document).height(),
-                    s = n(document).width()
-            }),
-            requestAnimationFrame(d)
+            polygon: {
+                nb_sides: 5
+            },
+            image: {
+                src: "https://cdn.matteobruni.it/images/particles/github.svg",
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 3,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 50,
+            random: {
+                enable: true,
+                minimumValue: 10
+            },
+            animation: {
+                enable: true,
+                speed: 5,
+                minimumValue: 10,
+                sync: true,
+                startValue: "min",
+                destroy: "max"
+            }
+        },
+        links: {
+            enable: false
+        },
+        move: {
+            enable: true,
+            speed: 10,
+            direction: "none",
+            random: false,
+            straight: false,
+            outMode: "destroy",
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detectsOn: "window",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "trail"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 0.8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            },
+            trail: {
+                delay: 0.005,
+                quantity: 5
+            }
+        }
+    },
+    retina_detect: true,
+    background: {
+        color: "#000000",
+        image: "url('https://newevolutiondesigns.com/images/freebies/4k-wallpaper-1.jpg')",
+        position: "50% 50%",
+        repeat: "no-repeat",
+        size: "cover"
+    },
+    backgroundMask: {
+        enable: true,
+        cover: {
+            color: "#000000"
+        }
     }
-}(jQuery);
-
-$(window).load(function() {
-    setTimeout(function() {
-        $('.sakuraT').sakura('start', {
-            blowAnimations: [
-                'blow-hard-left',
-
-            ], // Horizontal movement animation names
-            className: 'sakura', // Class name to use
-            fallSpeed: 1, // Factor for petal fall speed
-            maxSize: 34, // Maximum petal size
-            minSize: 9, // Minimum petal size
-            newOn: 100 // Interval after which a new petal is added
-        });
-
-    }, 1000);
 });
